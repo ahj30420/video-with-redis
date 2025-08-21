8571cd33bf0e911e1e03f3955b86639db56c0fdf
ahj30420 <ahj30420@naver.com>
2025-08-21 23:14:53 +0900

    feat: Video 관련 도메인 추가

---
+++ src/main/java/com/project/mytv/domain/video/Video.java
@@ -0,0 +1,28 @@
+package com.project.mytv.domain.video;
+
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Video {
+    private String id;
+    private String title;
+    private String description;
+    private String thumbnailUrl;
+    private String fileUrl;
+    private String channelId;
+    private Long viewCount;
+    private Long likeCount;
+    private LocalDateTime publishedAt;
+
+    public void bindCount(long viewCount, long likeCount) {
+        this.viewCount = viewCount;
+        this.likeCount = likeCount;
+    }
+}
