cd2c35338cfc331fabbcd97594e966785bc454da
ahj30420 <ahj30420@naver.com>
2025-08-22 00:08:52 +0900

    feat: ResponseDto 추가

---
+++ src/main/java/com/project/mytv/adapter/in/api/dto/CommandResponse.java
@@ -0,0 +1,10 @@
+package com.project.mytv.adapter.in.api.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class CommandResponse {
+    private String id;
+}
